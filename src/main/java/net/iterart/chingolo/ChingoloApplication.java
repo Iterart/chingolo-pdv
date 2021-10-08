fca190f2971a2ee2033723a0b84e56369b2b1953
ander <ander3324@gmail.com>
2021-10-08 18:46:02 -0300

    First commit: navbar created.

---
+++ src/main/java/net/iterart/chingolo/ChingoloApplication.java
@@ -0,0 +1,13 @@
+package net.iterart.chingolo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChingoloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChingoloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/iterart/chingolo/ChingoloApplicationTests.java
@@ -0,0 +1,13 @@
+package net.iterart.chingolo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChingoloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
